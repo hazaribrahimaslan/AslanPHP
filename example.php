18bd82e4c61e2d3eaf61830de1550e7fe2860b62
İbrahim Aslan <163416301+ibrahimaslandev@users.noreply.github.com>
2024-03-18 03:56:57 +0300

    Delete example.php

--- example.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "database.php";
-include "calendar.php";
-class example{
-  public function __construct(){
-    $database = new database();
-    $connect = $database->database_connect("localhost","db_name","admin","password");
-  }
-}
