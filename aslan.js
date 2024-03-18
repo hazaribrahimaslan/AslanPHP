e6103aa8f77a9b4bfb140e51fad67368d5c210c4
İbrahim Aslan <163416301+ibrahimaslanwebtr@users.noreply.github.com>
2024-03-18 17:16:31 +0300

    Add files via upload

---
+++ aslan.js
@@ -0,0 +1,7 @@
+function open_navigation() {
+    document.getElementById("navigation").style.width = "100%";
+}
+
+function close_navigation() {
+    document.getElementById("navigation").style.width = "0";
+}
